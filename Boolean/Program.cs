932a40b25813bf322cf9066b39b4ec7a87a07581
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-12-26 16:30:06 +0500

    Boolean

---
+++ Boolean/Program.cs
@@ -0,0 +1,121 @@
+﻿// Boolean 1
+/*
+int A;
+bool Tekshir;
+
+Console.WriteLine("Bitta butun son kiriting");
+A=Convert.ToInt32(Console.ReadLine());
+
+Tekshir = A > 0;
+Console.WriteLine(Tekshir);
+*/
+
+// Boolean 2
+/*
+int A;
+bool Tekshir;
+
+Console.WriteLine("Bitta butun son kiriting");
+A = Convert.ToInt32(Console.ReadLine());
+
+Tekshir = A %2==1;
+Console.WriteLine(Tekshir);
+*/
+
+// Boolean 3
+/*
+int A;
+bool Tekshir;
+
+Console.WriteLine("Bitta butun son kiriting");
+A = Convert.ToInt32(Console.ReadLine());
+
+Tekshir = A % 2 == 0;
+Console.WriteLine(Tekshir);
+*/
+
+//Boolean 4;
+/*
+int A, B;
+bool Tekshir;
+Console.WriteLine("Birinchi sonni kiriting");
+A=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ikkinchi sonni kiriting");
+B = Convert.ToInt32(Console.ReadLine());
+
+Tekshir = A > -2 & B <= 3;
+Console.WriteLine(Tekshir);
+
+*/
+
+// Boolean 6
+/*
+int A, B, C;
+bool Tekshir;
+Console.WriteLine("Birinchi sonni kiriting");
+A=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ikkinchi sonni kiriting");
+B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Uchinchi sonni kiriting");
+C = Convert.ToInt32(Console.ReadLine());
+Tekshir = A <= B & B <= C;
+Console.WriteLine(Tekshir);
+// A<=B<=C
+*/
+
+// Boolean 9
+/*
+int A, B;
+bool Tekshir;
+Console.WriteLine("Birinchi sonni kiriting");
+A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ikkinchi sonni kiriting");
+B = Convert.ToInt32(Console.ReadLine());
+Tekshir = A % 2 == 1 | B % 2 == 1;
+Console.WriteLine(Tekshir);
+
+*/
+
+// Boolean 10
+/*
+int A, B;
+bool Tekshir;
+Console.WriteLine("Birinchi sonni kiriting");
+A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ikkinchi sonni kiriting");
+B = Convert.ToInt32(Console.ReadLine());
+Tekshir = (A % 2 == 1 & B%2 == 0) |( A%2 == 0 & B % 2 == 1);
+Console.WriteLine(Tekshir);
+*/
+
+// Boolean 12
+/*
+int A, B, C;
+bool Tekshir;
+Console.WriteLine("Birinchi sonni kiriting");
+A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ikkinchi sonni kiriting");
+B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Uchinchi sonni kiriting");
+C = Convert.ToInt32(Console.ReadLine());
+Tekshir = A > 0 & B>0 & C>0 ;
+Console.WriteLine(Tekshir);
+*/
+
+// Boolean 14
+/*
+int A, B, C;
+bool Tekshir;
+Console.WriteLine("Birinchi sonni kiriting");
+A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ikkinchi sonni kiriting");
+B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Uchinchi sonni kiriting");
+C = Convert.ToInt32(Console.ReadLine());
+Tekshir = A > 0 & B < 0 & C < 0 
+    | A <0 & B > 0 & C < 0
+    | A < 0 & B < 0 & C > 0;
+Console.WriteLine(Tekshir);
+
+*/
+
